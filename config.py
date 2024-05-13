7303a1dc54d7de51b888f573c79ad76c19847da8
baitergg <vanapelme370@gmail.com>
2024-05-13 18:23:02 +0500

    first_commit

---
+++ config.py
@@ -0,0 +1,16 @@
+TELEGRAM_TOKEN = '7172555571:AAFv-kBq76WiP879ImqDBWL_C9JEh17V4y8'
+FOLDER_ID = 'b1gsfaui04sjkhsil864'
+IAM_TOKEN = 't1.9euelZrGx4mLlZTPjJOZipmcz5XIze3rnpWancyZx5PNxseej5bLmJaOzZXl9PcMKAhO-e9SNnK73fT3TFYFTvnvUjZyu83n9euelZqLjZ6Mi8mLi4-Xk5KXyo6YlO_8xeuelZqLjZ6Mi8mLi4-Xk5KXyo6YlL3rnpWamIuQzZePxpHHko_HzpSUnc613oac0ZyQko-Ki5rRi5nSnJCSj4qLmtKSmouem56LntKMng.3scIPIDcT3Sg_fG4i0jGd3EaEvrQezgynnlsdt3NR4smSzVyoX1IGB6f7EB6Q_GOzeLuV6MOagCZEP7VgMBwAQ'
+COUNT_LAST_MSG = 4
+MAX_USER_TTS_SYMBOLS = 1000
+MAX_TTS_SYMBOLS = 200
+MAX_USER_STT_BLOCKS = 12
+MAX_USERS = 3
+MAX_GPT_TOKENS = 120
+MAX_USER_GPT_TOKENS = 2_000
+SYSTEM_PROMPT = [{'role': 'system',
+                  'text': 'Ты умный и дружелюбный собеседник. '
+                          'Общайся с пользователем на "ты" и используй юмор.'
+                          'Поддерживай диалог. Изображай человека'}]
+LOGS = 'logs.txt'
+PATH_TO_DB = 'messages.db'
